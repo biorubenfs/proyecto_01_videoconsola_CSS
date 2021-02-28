24ee00170461a14ff143ebf1ab211fbfa8072708
biorubenfs <biorubenfs@hotmail.com>
2021-02-28 11:48:37 +0100

    Se ha agregado animacion de encendido, con encendido de piloto rojo, aparicion de logo y sonido clasico

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Proyecto 01 videoconsola CSS
 
 Réplica con HTML y CSS de la apariencia de una GameBoy clásica
+
+## Captura de pantalla
--- js/main.js
+++ js/main.js
@@ -1,7 +1,33 @@
+let isOn = false;
+
+// Función que enciende y apaga la consola. Tiene consecuencias en la luz del botón de battery, hace aparecer el logo de nintendo y reproduce el sonido clásico de inicio
 function encender() {
     let luz = document.getElementById("piloto-luz");
-    console.log("La función ha sido llamada");
+    let logo = document.getElementById("logo");
 
-    luz.style.background = "red";
-    luz.style.boxShadow = "0px 0px 4px 1px grey";
+    let sonido = document.getElementById("sonido");
+
+    if (!isOn) {
+        console.log("Encendiendo la consola...");
+
+        luz.style.background = "red";
+        luz.style.boxShadow = "0px 0px 4px 1px grey";
+
+        logo.style.display = "block";
+
+        sonido.play();
+
+        isOn = true;
+    }
+    else {
+        console.log("Apagando la consola");
+
+        luz.style.background = "maroon";
+        luz.style.boxShadow = "none";
+
+        logo.style.display = "none";
+
+        isOn = false;
+    }
+
 }
